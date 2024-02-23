565f9c28d163be1a3e231bd2bde0e62f3846f724
vinitjindam75 <69238936+vinitjindam@users.noreply.github.com>
2024-02-23 12:57:48 +0530

    Add files via upload

---
+++ build/precache-manifest.b929b6e4768abe544ff9b74b230a308d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4496b4548ce65af96f73788a5b9b13ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1741816c4ce497ebe95",
+    "url": "/static/css/main.8096fcf7.chunk.css"
+  },
+  {
+    "revision": "3682e4e4c7339046cc8c",
+    "url": "/static/js/2.b8c52690.chunk.js"
+  },
+  {
+    "revision": "c1741816c4ce497ebe95",
+    "url": "/static/js/main.dffe5bbb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
